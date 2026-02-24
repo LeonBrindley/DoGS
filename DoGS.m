aaeb30235af297713bf77f284e37a6cb64b69fc3
Leon Brindley <lpb32@cam.ac.uk>
2026-02-24 14:13:58 +0000

    Started implementing DoGS.m.

---
+++ DoGS.m
@@ -0,0 +1,4 @@
+%% 1. Clear any old variables and command window outputs, and close any old figures.
+clearvars; clc; close all;
+%% 2. Add any required directories to MATLAB's path.
+addpath(genpath(fileparts(mfilename('fullpath'))));
