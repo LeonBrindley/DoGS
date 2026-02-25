59f4149956c8cadd9570de85e17696314a22a39c
Leon Brindley <lpb32@cam.ac.uk>
2026-02-25 19:45:49 +0000

    Finished implementing UpdateVisibleFittingDefinitions.

--- GUI/ConfigurationWindow/Callback/UpdateVisibleFittingDefinitions.m
+++ GUI/ConfigurationWindow/Callback/UpdateVisibleFittingDefinitions.m
@@ -3,11 +3,11 @@ function State = UpdateVisibleFittingDefinitions(State)
     %% 1. Fetch the current table.
     Table = State.ConfigurationWindow.FittingDefinitionsSection.Table;
     %% 2. Save any edits from the old visible rows.
-    Table.UserData.AllRows(Table.UserData.VisibleRowIndices, :) = Table.Data;
+    Table.UserData.AllRows(Table.UserData.VisibleRows, :) = Table.Data;
     %% 3. Identify the new visible requirements.
     VisibleRequirements = [{'Any'}, State.Variables.Options.Models(:)', intersect(State.Variables.Options.GapTypes, {'Soft'})];
     %% 4. Identify the new visible rows.
-    Table.UserData.VisibleRowIndices = find(ismember(Table.UserData.AllRows(:, 2), VisibleRequirements));
-    %% 5. Update the visible rows.
-    Table.Data = Table.UserData.AllRows(Table.UserData.VisibleRowIndices, :);
+    Table.UserData.VisibleRows = find(ismember(Table.UserData.AllRows(:, 2), VisibleRequirements));
+    %% 5. Update the visible rows accordingly.
+    Table.Data = Table.UserData.AllRows(Table.UserData.VisibleRows, :);
 end
