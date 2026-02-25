94600559f4ab9651712865807549483f175c6f19
Leon Brindley <lpb32@cam.ac.uk>
2026-02-25 19:00:59 +0000

    Finished implementing OptionsSectionCallback.

--- GUI/ConfigurationWindow/Callback/OptionsSectionCallback.m
+++ GUI/ConfigurationWindow/Callback/OptionsSectionCallback.m
@@ -2,21 +2,12 @@
 function OptionsSectionCallback(Source, ~)
     %% 1. Extract the configuration window figure.
     Figure = ancestor(Source, 'figure');
-    %% 2. Store the previous options.
-    PreviousOptions = Figure.UserData.Variables.Options;
-    %% 3. Read the check boxes into State.Variables.Options.
-    State = OptionsSectionToVariables(Figure.UserData);
-    %% 4. Preserve the previous options in each category with none selected.
-    for idxCategory = 1 : State.Constants.PossibleOptions.NumCategories
-        CategoryName = State.Constants.PossibleOptions.Categories{idxCategory};
-        if(isempty(State.Variables.Options.(CategoryName)))
-            State.Variables.Options.(CategoryName) = PreviousOptions.(CategoryName);
-        end
-    end
-    %% 5. Update the visible fitting definitions.
+    %% 2. Read the check boxes into State.Variables.Options.
+    State = OptionsSectionToVariables(Figure.UserData, Figure.UserData.Variables.Options);
+    %% 3. Update the visible fitting definitions.
     State = UpdateVisibleFittingDefinitions(State);
-    %% 6. Update the visible solvers.
+    %% 4. Update the visible solvers.
     State = UpdateVisibleSolvers(State);
-    %% 7. Update the configuration window figure user data.
+    %% 5. Update the configuration window figure user data.
     Figure.UserData = State;
 end
