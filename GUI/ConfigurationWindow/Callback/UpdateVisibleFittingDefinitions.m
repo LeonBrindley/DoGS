490f089e48ff2811394d50a71b93d79ccb5b0e1b
Leon Brindley <lpb32@cam.ac.uk>
2026-02-25 19:19:08 +0000

    Continued implementing UpdateVisibleFittingDefinitions.

--- GUI/ConfigurationWindow/Callback/UpdateVisibleFittingDefinitions.m
+++ GUI/ConfigurationWindow/Callback/UpdateVisibleFittingDefinitions.m
@@ -3,18 +3,16 @@ function State = UpdateVisibleFittingDefinitions(State)
     %% 1. Extract the table data.
     Table = State.ConfigurationWindow.FittingDefinitionsSection.Table;
     if(isempty(Table.UserData))
-        Table.UserData = struct('FullData', {Table.Data}, 'FullNames', {Table.Data(:, [2 1])});
+        Table.UserData = struct('FullData', {Table.Data}, 'VisibleIndices', {(1 : size(Table.Data, 1))'});
     end
     %% 2. Update the cached data.
-    for idxRow = 1 : size(Table.Data, 1)
-        CachedRow = strcmp(Table.UserData.FullNames(:, 1), Table.Data{idxRow, 2}) & strcmp(Table.UserData.FullNames(:, 2), Table.Data{idxRow, 1});
-        Table.UserData.FullData(CachedRow, :) = Table.Data(idxRow, :);
-    end
+    Table.UserData.FullData(Table.UserData.VisibleIndices, :) = Table.Data;
     %% 3. Determine the visible requirements.
     VisibleRequirements = [{'Any'}, State.Variables.Options.Models(:)'];
     if(any(strcmp(State.Variables.Options.GapTypes, 'Soft')))
         VisibleRequirements = [VisibleRequirements, {'Soft'}];
     end
     %% 4. Update the table data.
-    Table.Data = Table.UserData.FullData(ismember(Table.UserData.FullNames(:, 1), VisibleRequirements), :);
+    Table.UserData.VisibleIndices = find(ismember(Table.UserData.FullData(:, 2), VisibleRequirements));
+    Table.Data = Table.UserData.FullData(Table.UserData.VisibleIndices, :);
 end
