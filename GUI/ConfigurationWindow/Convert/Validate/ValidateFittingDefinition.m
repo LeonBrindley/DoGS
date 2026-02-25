e73dad1a926bb392026279f41c9338e85cadb44e
Leon Brindley <lpb32@cam.ac.uk>
2026-02-25 15:40:37 +0000

    Continued implementing ValidateFittingDefinition.

--- GUI/ConfigurationWindow/Convert/Validate/ValidateFittingDefinition.m
+++ GUI/ConfigurationWindow/Convert/Validate/ValidateFittingDefinition.m
@@ -1,6 +1,10 @@
 %% Function: Validate a Fitting Definition.
 function FittingDefinition = ValidateFittingDefinition(FittingDefinition, PreviousFittingDefinition)
     %% 1. Determine whether a previous fitting definition has been specified.
-    HasPreviousFittingDefinition = (nargin >= 4 && ~isempty(PreviousFittingDefinition));
+    HasPreviousFittingDefinition = (nargin >= 2 && ~isempty(PreviousFittingDefinition));
     FailureMessage = '';
+    %% 2. Validate that the scale, initial value, lower bound and upper bound are finite.
+    if(any(~cellfun(@(Value) isnumeric(Value) && all(isfinite(Value), 'all'), FittingDefinition(3 : 6))))
+        FailureMessage = ' is not a finite scalar or vector.';
+    end
 end
