23275865ec16ac42c769f0532f92af5dc9d64c2b
Leon Brindley <lpb32@cam.ac.uk>
2026-02-26 14:50:45 +0000

    Continued implementing UpdateVisibleSolvers.

--- GUI/ConfigurationWindow/Callback/UpdateVisibleFittingDefinitions.m
+++ GUI/ConfigurationWindow/Callback/UpdateVisibleFittingDefinitions.m
@@ -2,7 +2,7 @@
 function State = UpdateVisibleFittingDefinitions(State)
     %% 1. Fetch the current table.
     Table = State.ConfigurationWindow.FittingDefinitionsSection.Table;
-    %% 2. Identify the new visible requirements.
+    %% 2. Identify the visible requirements.
     VisibleRequirements = [{'Any'}, State.Variables.Options.Models(:)', intersect(State.Variables.Options.GapTypes, {'Soft'})];
     %% 3. Update the visible rows accordingly.
     UpdateVisibleRows(Table, VisibleRequirements);
--- GUI/ConfigurationWindow/Callback/UpdateVisibleSolvers.m
+++ GUI/ConfigurationWindow/Callback/UpdateVisibleSolvers.m
@@ -2,8 +2,16 @@
 function State = UpdateVisibleSolvers(State)
     %% 1. Fetch the current table.
     Table = State.ConfigurationWindow.SolversSection.Table;
-    %% 2. Identify the new visible requirements.
+    %% 2. Identify the visible requirements.
     VisibleRequirements = [{'Any'}, State.Variables.Options.Solvers(:)'];
-    %% 3. Update the visible rows accordingly.
+    %% 3. If 'fmincon' is used as a hybrid function, update the visible requirements accordingly.
+    if(any(ismember(State.Variables.Options.Solvers, {'ga', 'particleswarm'})))
+        VisibleRequirements = [VisibleRequirements, {'fmincon'}];
+    end
+    %% 4. Update the visible rows accordingly.
     UpdateVisibleRows(Table, VisibleRequirements);
+    %% 5. If 'fmincon' is not used as a primary function, hide NumberOfStartPoints accordingly.
+    if(~any(strcmp(State.Variables.Options.Solvers, 'fmincon')))
+        Table.Data(strcmp(Table.Data(:, 2), 'fmincon') & strcmp(Table.Data(:, 1), 'NumberOfStartPoints'), :) = [];
+    end
 end
