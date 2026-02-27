e23c06b2853b1067b182cb203924934026362c9e
Leon Brindley <lpb32@cam.ac.uk>
2026-02-27 13:39:23 +0000

    Continued implementing EvaluateOptimiser.

--- Dependents/EvaluateOptimiser.m
+++ Dependents/EvaluateOptimiser.m
@@ -9,7 +9,18 @@ function State = EvaluateOptimiser(State)
     end
     Requirements{end + 1} = State.Dependents.Model;
     %% 3. Convert the initial values, lower bounds and upper bounds from structs into three arrays.
-
+    State.Dependents.Optimiser.InitialValues = [];
+    State.Dependents.Optimiser.LowerBounds = [];
+    State.Dependents.Optimiser.UpperBounds = [];
+    for idxRequirement = 1 : numel(Requirements)
+        ParameterNames = fieldnames(State.Variables.FittingDefinitions.(Requirements{idxRequirement}));
+        for idxParameter = 1 : numel(ParameterNames)
+            FittingDefinition = State.Variables.FittingDefinitions.(Requirements{idxRequirement}).(ParameterNames{idxParameter});
+            State.Dependents.Optimiser.InitialValues = [State.Dependents.Optimiser.InitialValues, reshape(FittingDefinition{2} ./ FittingDefinition{1}, 1, [])];
+            State.Dependents.Optimiser.LowerBounds = [State.Dependents.Optimiser.LowerBounds, reshape(FittingDefinition{3} ./ FittingDefinition{1}, 1, [])];
+            State.Dependents.Optimiser.UpperBounds = [State.Dependents.Optimiser.UpperBounds, reshape(FittingDefinition{4} ./ FittingDefinition{1}, 1, [])];
+        end
+    end
     %% 4. Configure the options associated with the solver in question.
 
     %% 5. Define the objective function to be minimised.
